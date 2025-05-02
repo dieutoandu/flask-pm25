a5287dcd94274090949a795316d621c1099e1b94
dieutoandu <dieutoandu@gmail.com>
2025-05-02 22:03:54 +0800

    '5_2'

--- app.py
+++ app.py
@@ -1,21 +1,56 @@
 from flask import Flask, render_template, request
 from datetime import datetime
 import pandas as pd
-from pm25 import get_pm25_data_from_mysql, update_db
+from pm25 import (
+    get_pm25_data_from_mysql,
+    update_db,
+    get_pm25_data_by_site,
+    get_all_counties,
+    get_site_by_county,
+)
 import json
 
 app = Flask(__name__)
 
 
-@app.route("/filter", methods=["POST"])
-def filter():
-    county = request.form.get("county")
-    datas, columns = get_pm25_data_from_mysql()
-    df = pd.DataFrame(datas, columns=columns)
+@app.route("/pm25-county-site")
+def pm25_county_site():
+    county = request.args.get("county")
+    site = get_site_by_county(county)
 
-    df1 = df.groupby("county").get_group(county).groupby("site")["pm25"].mean()
-    print(df1)
-    return {"county": county}
+    result = json.dumps(site, ensure_ascii=False)
+
+    return result
+
+
+@app.route("/pm25-site")
+def pm25_site():
+    counties = get_all_counties()
+
+    return render_template("pm25-site.html", counties=counties)
+
+
+@app.route("/pm25-data-site")
+def pm25_data_by_site():
+    county = request.args.get("county")
+    site = request.args.get("site")
+
+    if not county or not site:
+        result = {"erro": "county and site False"}
+    else:
+        datas, columns = get_pm25_data_by_site(county, site)
+        df = pd.DataFrame(datas, columns=columns)
+        date = df["datacreationdate"].apply(lambda x: x.strftime("%Y-%m-%d %H"))
+
+        data = {
+            "county": county,
+            "site": site,
+            "x_data": date.to_list(),
+            "y_data": df["pm25"].to_list(),
+        }
+
+        result = json.dumps(data, ensure_ascii=False)
+    return result
 
 
 @app.route("/update-db")
@@ -46,13 +81,11 @@ def index():
         x_data = df1["county"].tolist()
 
     else:
-
         df = df.groupby("county").get_group(county)
         x_data = df["site"].tolist()
 
     columns = df.columns.tolist()
     datas = df.values.tolist()
-
     y_data = df["pm25"].tolist()
 
     return render_template(
@@ -64,63 +97,6 @@ def index():
         x_data=x_data,
         y_data=y_data,
     )
-
-
-@app.route("/books")
-def books_page():
-    books = [
-        {
-            "name": "Python book",
-            "price": 299,
-            "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
-        },
-        {
-            "name": "Java book",
-            "price": 399,
-            "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
-        },
-        {
-            "name": "C# book",
-            "price": 499,
-            "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
-        },
-    ]
-
-    # books = []
-    if books:
-        for key in books:
-            print(key["name"])
-            print(key["price"])
-            print(key["image_url"])
-    else:
-        print("is no any books:")
-
-    # return f"<h1>HELLO WORLD</h1><br>{datetime.now()}"
-    username = "irving"
-    nowtime = datetime.now().strftime("%Y-%m-%d")
-    books = books
-    print(username, nowtime)
-    return render_template("books.html", name=username, now=nowtime, books=books)
-
-
-@app.route("/bmi")
-def get_bmi():
-    # args => GET
-    height = request.args.get("height")
-    weight = request.args.get("weight")
-
-    bmi = round(eval(weight) / (eval(height) / 100) ** 2, 2)
-
-    return render_template("bmi.html", **locals())
-
-
-@app.route("/pm25-data")
-def get_pm25_data():
-    api_url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=540e2ca4-41e1-4186-8497-fdd67024ac44&limit=1000&sort=datacreationdate%20desc&format=CSV"
-    df = pd.read_csv(api_url)
-    df["datacreationdate"] = pd.to_datetime(df["datacreationdate"])
-    df1 = df.dropna()
-    return df1.values.tolist()
 
 
 if __name__ == "__main__":
--- pm25.py
+++ pm25.py
@@ -2,6 +2,53 @@ import pandas as pd
 import pymysql
 
 
+def get_site_by_county(county):
+    conn = None
+    site = []
+    try:
+        conn = open_db()
+        cur = conn.cursor()
+
+        sqlstr = "select distinct site from pm25 where county=%s;"
+        cur.execute(sqlstr, (county,))
+        print(cur.description)
+        datas = cur.fetchall()
+        print(datas)
+        site = [data[0] for data in datas]
+
+    except Exception as e:
+        print(e)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return site
+
+
+def get_all_counties():
+
+    conn = None
+    counties = []
+    try:
+        conn = open_db()
+        cur = conn.cursor()
+
+        sqlstr = "select distinct county from pm25 ;"
+        cur.execute(sqlstr)
+        print(cur.description)
+        datas = cur.fetchall()
+        print(datas)
+        counties = [data[0] for data in datas]
+
+    except Exception as e:
+        print(e)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return counties
+
+
 def update_db():
     api_url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=540e2ca4-41e1-4186-8497-fdd67024ac44&limit=1000&sort=datacreationdate%20desc&format=CSV"
     sqlstr = """
@@ -45,6 +92,27 @@ def open_db():
     return conn
 
 
+def get_pm25_data_by_site(county, site):
+    datas = None
+    conn = None
+    columns = None
+    try:
+        conn = open_db()
+        cur = conn.cursor()
+
+        sqlstr = "select * from pm25 where county=%s and site=%s;"
+        cur.execute(sqlstr, (county, site))
+        print(cur.description)
+        columns = [col[0] for col in cur.description]
+        datas = cur.fetchall()
+    except Exception as e:
+        print(e)
+    finally:
+        if conn is not None:
+            conn.close()
+    return datas, columns
+
+
 def get_pm25_data_from_mysql():
     datas = None
     conn = None
@@ -55,6 +123,7 @@ def get_pm25_data_from_mysql():
         # sqlstr = "select MAX(datacreationdate) from pm25;"
 
         sqlstr = "select * from pm25 where datacreationdate=(select MAX(datacreationdate) from pm25);"
+
         cur.execute(sqlstr)
         print(cur.description)
         columns = [col[0] for col in cur.description]
@@ -70,4 +139,4 @@ def get_pm25_data_from_mysql():
 if __name__ == "__main__":
     # datas, columns = get_pm25_data_from_mysql()
     # print(columns)
-    print(update_db())
+    print(get_site_by_county("新北市"))
