23c6e62c0e307df0300644eb61b7a89901031465
dieutoandu <dieutoandu@gmail.com>
2025-05-07 19:43:50 +0800

    end_05_07

--- app.py
+++ app.py
@@ -47,6 +47,8 @@ def pm25_data_by_site():
             "site": site,
             "x_data": date.to_list(),
             "y_data": df["pm25"].to_list(),
+            "pm25_max": df["pm25"].max(),
+            "pm25_min": df["pm25"].min(),
         }
 
         result = json.dumps(data, ensure_ascii=False)
